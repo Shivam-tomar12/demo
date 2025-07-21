f4a5c3033c860f97b8cca5a2a2671e09bd4a0168
Shivam Tomar <12shivamt@gmail.com>
2025-07-21 16:21:12 +0530

    qa chanes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,8 @@ public class DemoApplication {
 		System.out.println("Hello world");
 		System.out.println("hi");
 		System.out.println("Shivam ");
+		System.out.println("tomar  ");
+		System.out.println("tomar ji ");
 		System.out.println("Shivam tomar");
 		System.out.println("Shivam practice");
 		System.out.println("Shivam test 1");
